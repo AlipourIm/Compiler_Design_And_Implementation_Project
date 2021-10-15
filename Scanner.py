cd543293fba0d49c6c9dce8b9e0b206fee6ddb6c
Iman <iman.alip2001@gmail.com>
2021-10-15 21:48:18 +0330

    Added scanner buffer. Changed position attribute to buffer_pointer attribute. Added dfa_table to Scanner constructor. Fixed dfa_table content types from string to int.

--- Scanner.py
+++ Scanner.py
@@ -1,9 +1,21 @@
+import os
+
+
 class Scanner:
     def __init__(self):
-        self.pos = 0
+        self.buffer_pointer = 0
+        self.buffer = ""
         self.lexeme = ""
         self.line = 0
         self.file = open("input.txt", "r")
+        self.dfa_table = []
+        with open(os.path.join(os.path.dirname(__file__), "preprocess/DFA_Table.txt"), "r") as f:
+            for line in f:
+                self.dfa_table.append(line.strip('][').replace('"', '').split(','))
+        for i in range(20):
+            self.dfa_table[i][255] = self.dfa_table[i][255].replace("]\n", "")
+        f.close()
+        self.dfa_table = [list(map(int, i)) for i in self.dfa_table]
 
     def get_line(self):
         return self.line
@@ -20,4 +32,4 @@ class Scanner:
     def get_next_token(self):
         pass                    # To be implemented
 
-
+scanner = Scanner()
