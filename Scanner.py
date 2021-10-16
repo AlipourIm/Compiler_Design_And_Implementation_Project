36d822b5ea13356b77c2e89e88e80c411227bd80
Iman <iman.alip2001@gmail.com>
2021-10-16 16:41:00 +0330

    Started writing get_next_token function.

--- Scanner.py
+++ Scanner.py
@@ -5,12 +5,11 @@ class Scanner:
     def __init__(self):
         self.buffer_pointer = 0
         self.buffer = ""
-        self.lexeme = ""
         self.line = 0
-        self.current_state = 0
         self.file = open("input.txt", "r")
-        self.final_states, self.look_ahead_states = Scanner.final_state_initializer()
+        self.final_states, self.final_state_message, self.look_ahead_states = Scanner.final_state_initializer()
         self.dfa_table = Scanner.dfa_initializer()
+        self.refill_buffer()
 
     @staticmethod
     def dfa_initializer():
@@ -27,18 +26,19 @@ class Scanner:
     @staticmethod
     def final_state_initializer():
         final_states = []
-        look_ahead_states = []
+        final_states_message = []
         with open(os.path.join(os.path.dirname(__file__), "preprocess/final_states.txt"), "r") as f:
             for line in f:
                 items = line.split(", ")
                 items[0] = int(items[0].replace("[", ""))
                 items[1] = items[1].replace("]", "")
-                final_states.append(items)
+                final_states.append(int(items[0]))
+                final_states_message.append(items[1])
             f.close()
         with open(os.path.join(os.path.dirname(__file__), "preprocess/look_ahead_states.txt"), "r") as f:
             look_ahead_states = list(map(int, f.readline().split()))
-            f.close
-            return final_states, look_ahead_states
+            f.close()
+            return final_states, final_states_message, look_ahead_states
 
     def refill_buffer(self):
         self.buffer = "" + self.file.read(32)
@@ -57,8 +57,20 @@ class Scanner:
         return self.file.read(1)
 
     def get_next_token(self):
-        self.current_state = 0
-        self.lexeme = ""
+        current_state = 0
+        lexeme = ""
+        while not (current_state in self.look_ahead_states or current_state in self.final_states):
+            lexeme += self.buffer[self.buffer_pointer]
+            current_state = self.dfa_table[current_state][ord(self.buffer[self.buffer_pointer])]
+            self.buffer_pointer += 1
+            if self.buffer_pointer == 32:
+                self.refill_buffer()                                                                # Buffer has EOF, my idea: use a flag.
+        if current_state in self.look_ahead_states:                                                 # How to look ahead?
+            self.buffer_pointer -= 1
+            lexeme = lexeme[:-1]
+        return lexeme, self.final_state_message[self.final_states.index(current_state)]
 
 
 scanner = Scanner()
+for i in range(10):
+    print(scanner.get_next_token())
