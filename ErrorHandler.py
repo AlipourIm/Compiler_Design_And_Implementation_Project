2e036d50eba438fe4282478404e14a865213575b
Iman <iman.alip2001@gmail.com>
2021-10-17 08:54:12 +0330

    Added Error Handler's write lexical_error to file function, this writes errors one by one once found. Added Symbol Table's symbol table. Added symbol table's add symbol function. Added symbol table's print_symbol function, this writes the whole file all together and once. All of mentioned functions and fields are static.

--- ErrorHandler.py
+++ ErrorHandler.py
@@ -1,9 +1,16 @@
 class ErrorHandler:
+    f = open("lexical_errors.txt", "w")
+
     def __init__(self):
         pass
 
     def catch_error(self, error_name, characters_of_error, line_of_error):
         pass
 
-    def print_lexical_errors_phase1(self):
-        pass
+    @staticmethod
+    def print_lexical_error(line, lexeme, message):
+        ErrorHandler.f.write(str(line) + ".\t(" + lexeme + " " + message + ")\n")
+
+    @staticmethod
+    def close_file():
+        ErrorHandler.f.close()
--- SymbolTable.py
+++ SymbolTable.py
@@ -1,4 +1,7 @@
 class SymbolTable:
+    symbol_table = ["if", "else", "void", "int", "repeat", "break", "until", "return"]
+    f = open("symbol-table.txt", "w")
+
     def __init__(self):
         pass
 
@@ -7,3 +10,17 @@ class SymbolTable:
 
     def print_table_phase1(self):
         pass
+
+    @staticmethod
+    def add_symbol(symbol):
+        if not (symbol in SymbolTable.symbol_table):
+            SymbolTable.append(symbol)
+
+    @staticmethod
+    def print_symbols():
+        for i in range(len(SymbolTable.symbol_table)):
+            SymbolTable.f.write(str(i + 1) + ".\t" + SymbolTable.symbol_table[i] + "\n")
+
+    @staticmethod
+    def close_file():
+        SymbolTable.f.close()
