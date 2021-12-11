89e4c15f1afee3c9b151ee184edcc8d356e9688d
AlipourIm <iman.alip2001@gmail.com>
2021-12-11 12:32:28 +0330

    added error handler function that writes syntax errors. added dump_parse_tree function in parser. reformatted ActionTable.py.

--- Parser.py
+++ Parser.py
@@ -1,15 +1,18 @@
 from ErrorHandler import ErrorHandler
 from Scanner import Scanner
 from SymbolTable import SymbolTable
+from anytree import Node, RenderTree
 
 
 class Parser:
 
     def __init__(self):
         self.f = open("tokens.txt", "w")
+        self.parse_tree_file = open("parse_tree.txt", "w")
         self.tokens = []
         self.line = -1
         self.tokens_in_line = ""
+        self.parse_tree = None
 
     def main(self):
         scanner = Scanner()
@@ -35,3 +38,8 @@ class Parser:
         SymbolTable.print_symbols()
 
         self.f.close()
+
+    def dump_parse_tree(self):
+        for pre, fill, node in RenderTree(self.parse_tree):
+            self.parse_tree_file.write("%s%s" % (pre, node.name))
+        self.parse_tree_file.close()
